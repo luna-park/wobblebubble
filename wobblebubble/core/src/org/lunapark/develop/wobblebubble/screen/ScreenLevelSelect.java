9fb017d5df9c912793ec0388f9e76f7ea641efe9
znak <tynuza@gmail.com>
2014-11-16 22:05:37 +0300

    Commit changes: add sfx

---
+++ wobblebubble/core/src/org/lunapark/develop/wobblebubble/screen/ScreenLevelSelect.java
@@ -0,0 +1,13 @@
+package org.lunapark.develop.wobblebubble.screen;
+
+import org.lunapark.develop.wobblebubble.base.ScreenBase;
+
+import com.badlogic.gdx.Game;
+
+public class ScreenLevelSelect extends ScreenBase {
+
+	public ScreenLevelSelect(Game game) {
+		super(game);		
+	}
+
+}
